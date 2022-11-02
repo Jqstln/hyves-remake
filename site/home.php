5e73e6a8266ef8695c12792cf36003ef68cca127
Justinnn <63072677+Jqstln@users.noreply.github.com>
2022-11-02 13:13:56 +0100

    feat: voeg homepage

---
+++ site/home.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+ 
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hyves - Home</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <style>
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <h1 class="my-5">Hallo, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>.<br> Welkom bij Hyves.</h1>
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Reset jou wachtwoord.</a>
+        <a href="logout.php" class="btn btn-danger ml-3">Uitloggen.</a>
+    </p>
+</body>
+</html>
