d0c360ef37248897d465d674b2cd0631039685e9
Justinnn <63072677+Jqstln@users.noreply.github.com>
2022-11-02 13:14:22 +0100

    feat: mogelijkheid om te registeren

---
+++ site/register.php
@@ -0,0 +1,119 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config/database.php';
+ 
+$username = $password = $confirm_password = "";
+$username_err = $password_err = $confirm_password_err = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Validate username
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Voer een gebruikers naam in.";
+    } elseif(!preg_match('/^[a-zA-Z0-9]+$/', trim($_POST["username"]))){
+        $username_err = "Gebruikersnaam mag alleen letters en cijfers bevatten.";
+    } else{
+        $sql = "SELECT id FROM users WHERE username = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            $param_username = trim($_POST["username"]);
+            
+            if(mysqli_stmt_execute($stmt)){
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $username_err = "Deze gebruikersnaam bestaat al.";
+                } else{
+                    $username = trim($_POST["username"]);
+                }
+            } else{
+                echo "Oeps! Er is iets fout gegaan. Probeer het later opnieuw.";
+            }
+
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Voer een wachtwoord in.";     
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "Wachtwoord moet minimaal 6 tekens bevatten.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Bevestig het wachtwoord.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Wachtwoord komt niet overeen.";
+        }
+    }
+
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT);
+
+            if(mysqli_stmt_execute($stmt)){
+                header("location: login.php");
+            } else{
+                echo "Oeps! Er is iets fout gegaan. Probeer het later opnieuw.";
+            }
+
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hyves - Maak een account aan</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 360px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Account aanmaken</h2>
+        <p>Vul deze formulier in om uw account aan te maken</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label>Gebruikersnaam</label><br>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Wachtwoord</label><br>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <label>Bevestig wachtwoord</label><br>
+                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <input type="reset" class="btn btn-secondary ml-2" value="Reset">
+            </div>
+            <p>Heb je al een account? Log dan in via <a href="login.php">hier</a>.</p>
+        </form>
+    </div>    
+</body>
+</html>
